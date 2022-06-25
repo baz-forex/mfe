8816d2079c9121125f1b69dd692dada046c3af26
Miguel Angel García Santiago <midato.mx@gmail.com>
2022-06-25 13:35:10 -0500

    Created container deploy script

---
+++ packages/container/config/webpack.production.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+
+const domain = process.env.REACT_APP_PRODUCTION_DOMAIN_URL
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `my_marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
