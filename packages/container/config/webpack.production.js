b13acedd88a473fe7e1a0182676d3b81fda1f34b
Miguel Angel García Santiago <midato.mx@gmail.com>
2022-06-25 18:10:48 -0500

    [FIX] 1- Created container deploy script 15

--- packages/container/config/webpack.production.js
+++ packages/container/config/webpack.production.js
@@ -6,19 +6,20 @@ const packageJson = require('../package.json')
 const domain = process.env.REACT_APP_PRODUCTION_DOMAIN_URL
 
 const prodConfig = {
-  mode: 'production',
-  output: {
-    filename: '[name].[contenthash].js'
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: `my_marketing@${domain}/marketing/remoteEntry.js`
-      },
-      shared: packageJson.dependencies
-    })
-  ]
+	mode: 'production',
+	output: {
+		filename: '[name].[contenthash].js',
+		publicPath: '/container/latest/'
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: `my_marketing@${domain}/marketing/remoteEntry.js`
+			},
+			shared: packageJson.dependencies
+		})
+	]
 }
 
 module.exports = merge(commonConfig, prodConfig)
