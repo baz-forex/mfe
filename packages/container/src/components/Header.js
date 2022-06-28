b073f15e8e346762da6924dad5942409bc8c4fcf
Miguel Angel García Santiago <midato.mx@gmail.com>
2022-06-28 08:42:18 -0500

    [UPDATE] Title portal

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
 						component={RouterLink}
 						to="/"
 					>
-						App
+						Portal Divisas
 					</Typography>
 					<Button
 						color="primary"
